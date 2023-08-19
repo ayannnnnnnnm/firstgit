de7a306205ab974f2e31c73e6313b9b448ef45a0
ayannnnnnnnm <142567350+ayannnnnnnnm@users.noreply.github.com>
2023-08-19 11:22:18 +0530

    Add files via upload

---
+++ Addition of two numbers.py
@@ -0,0 +1,5 @@
+print("Enter two number that you to add")
+num1=int(input("Enter the number :"))
+num2=int(input("Enter the number :"))
+result=num1+num2
+print("Addition of two numbers are : ", result) 
